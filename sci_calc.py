47bc47fde2effc10c09e465f623418b4ab8e10f1
RoqayaAlmamari <roqayaalmamari@gmail.com>
2023-04-30 13:07:58 +0400

    add changes

--- sci_calc.py
+++ sci_calc.py
@@ -3,4 +3,15 @@ import math
 def multiply(num1, num2):
     result = num1 * num2
     return result
-print(multiply(2, 3))
+
+
+
+def get_pow(base, exp):
+    return math.pow(base, exp)
+
+
+# For unit tests
+if __name__ == "__main__":
+    print(get_pow(2, 3))
+    print(multiply(4, 4))
+
