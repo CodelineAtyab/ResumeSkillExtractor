cfd4b304752c3f1c32f712e3fe8b2cf16c8ecc82
BaraahAlKalbani <baraalkalbani@gmail.com>
2023-04-30 12:33:23 +0400

    div

--- sci_calc.py
+++ sci_calc.py
@@ -1,16 +1,13 @@
 import math
 
 
-def get_pow(base, exp):
-    return math.pow(base, exp)
-
-
-# For unit tests
-if __name__ == "__main__":
-    print(get_pow(2, 3))
-
-
 def get_dividend(dividend, divisor):
     if divisor == 0:
         raise ValueError("Divisor cannot be zero")
     return dividend / divisor
+
+
+# For unit tests
+if __name__ == "__main__":
+    print(get_dividend(10, 2))
+    print(get_dividend(5, 0))
