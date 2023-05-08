b4d3785cd4a47ed1ee900f250a19980730186d03
AnwaarAlbusaidi <AnwaarAlbusaidi5@gmail.com>
2023-05-08 09:21:54 +0400

    Done

--- src/Resume_Parser.py
+++ src/Resume_Parser.py
@@ -1,4 +1,5 @@
 from pyresparser import ResumeParser
+
 import nltk
 
 nltk.download('words')
@@ -6,14 +7,35 @@ nltk.download('stopwords')
 
 
 def parse_cv(path_to_cv):
-    # Create a ResumeParser object with the path to the PDF CV
-    parser = ResumeParser(path_to_cv)
+    """
+    Extracts data from a PDF resume file.
 
-    # Use the get_extracted_data() method of the parser object to extract data from the CV
-    extracted_data = parser.get_extracted_data()
+    Parameters:
+    path_to_cv (str): The path to the PDF resume file.
 
-    # Return the extracted data as a dictionary
-    return extracted_data
+    Returns:
+    dict: A dictionary containing extracted data, including name, email, phone number, skills, work experience, education, and more.
+
+    Raises:
+    FileNotFoundError: If the provided file path is invalid or the file does not exist.
+    ValueError: If the provided file is not a valid PDF.
+    Exception: If any other error occurs while parsing the resume file.
+    """
+    try:
+        # Create a ResumeParser object with the path to the PDF CV
+        parser = ResumeParser(path_to_cv)
+
+        # Use the get_extracted_data() method of the parser object to extract data from the CV
+        extracted_data = parser.get_extracted_data()
+
+        # Return the extracted data as a dictionary
+        return extracted_data
+    except FileNotFoundError:
+        print("Invalid file path. Please provide a valid path to a PDF file.")
+    except ValueError:
+        print("Invalid PDF file. Please provide a valid PDF file.")
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 # For unit tests
