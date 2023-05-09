14e31ff07ac094496f1f53041b02029aafeb80b6
FatemaAlbalushi <fatoomsaid188@gmail.com>
2023-05-09 15:59:14 +0400

    Input Data Scanning

--- input_data_scanner.py
+++ input_data_scanner.py
@@ -3,43 +3,54 @@ import queue
 
 class FileScanner:
     """
-      A class to scan a directory for new files with a specified file extension
-      and add their file paths to a queue.
+    A class to scan a directory for new files with a specified file extension
+    and add their file paths to a queue.
 
-      Attributes:
-      - existing_files (set): A set to hold the paths of existing files.
-      - cv_queue (queue.Queue): A queue to hold the paths of new files.
+    Attributes:
+    - existing_files (set): A set to hold the paths of existing files.
+    - cv_queue (queue.Queue): A queue to hold the paths of new files.
 
-      Methods:
-      - scan(): Scans the directory for new files and adds their file paths to the queue.
-      """
-    def __init__(self,cv_queue):
+    Methods:
+    - scan(): Scans the directory for new files and calls add_file_to_queue for each new file.
+    - add_file_to_queue(filepath): Adds a file path to the queue.
+    """
+    def __init__(self, cv_queue):
         """
-               Initializes a new instance of the FileScanner class.
+        Initializes a new instance of the FileScanner class.
 
-               Args:
-               - cv_queue (queue.Queue): A queue to hold the paths of new files.
+        Args:
+        - cv_queue (queue.Queue): A queue to hold the paths of new files.
         """
         # Create a set to hold the paths of existing files
+
         self.existing_files = set()
         # Create a queue to hold the paths of new files
-        self.cv_queue = queue.Queue()
+        self.cv_queue = cv_queue
+
+    def add_to_queue(self, filepath):
+        """
+        Adds a file path to the queue.
+
+        Args:
+        - filepath (str): The path of the file to be added to the queue.
+        """
+        print(f'Processing {filepath}')
+        print(f'Adding {filepath} to queue')
+        self.cv_queue.put(filepath)
 
     def scan(self):
         """
-              Scans the directory for new files and adds their file paths to the queue.
+        Scans the directory for new files and calls add_file_to_queue for each new file.
         """
         # Get a list of paths of all files with the desired file extension
         try:
-            filepaths = set(glob.glob('C:/Users/LAP-10/Documents/input_data/*.pdf'))
+            filepaths = set(glob.glob('./input_data/*.pdf'))
             # Get the set difference between the existing files and the new files
             new_files = filepaths - self.existing_files
 
             # Process each new file
             for filepath in new_files:
-                print(f'Processing {filepath}')
-                print(f'Adding {filepath} to queue')
-                self.cv_queue.put(filepath)
+                self.add_to_queue(filepath)
 
             # Update the set of existing files
             self.existing_files = filepaths
@@ -56,4 +67,3 @@ if __name__ == '__main__':
     scanner = FileScanner(cv_queue)
     # Call the scan method to start scanning the directory for new files
     scanner.scan()
-
