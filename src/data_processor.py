4ccad73756b3273b10d97c7d0b030ccc2aa1236c
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2024-02-11 11:01:33 +0400

    Producer now waits until all jobs are complete and then it displays the result

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,2 +1,3 @@
 def process_cv(cv_file_content):
+    import pyresparser
     return cv_file_content
--- src/producer.py
+++ src/producer.py
@@ -1,25 +1,39 @@
 import time
 import redis
 from rq import Queue
+from rq.job import Job
 
 from data_processor import process_cv
 
 
-def report_success(job, connection, result, *args, **kwargs):
-    print(f">>>>>>> Job {job.id} is completed. The result is: {result}")
+# Make a connection to the Redis server and grab the reference of the default queue.
+REDIS_CONNECTION = redis.Redis(host="localhost", port="6379")
+STORAGE_QUEUE = Queue(connection=REDIS_CONNECTION)
 
 
-redis_connection = redis.Redis(host="localhost", port="6379")
-storage_queue = Queue(connection=redis_connection)
+def all_jobs_done(list_of_jobs: list[Job]) -> bool:
+    """
+    :param list_of_jobs: List of Job objects that are referring to the scheduled jobs.
+    :return: False if any of the scheduled job is not finished. True if all jobs are complete.
+    """
+    for job in list_of_jobs:
+        if not job.is_finished:
+            return False
+    return True
+
 
 # TODO: Read CVs and put the content on the remote Queue, so it can be processed by the consumers
 cv_content_to_process = ["Javascript Developer", "Java Developer", "Python Developer", "C# Developer"]
 
 list_of_scheduled_jobs = []
 for curr_content in cv_content_to_process:
-    list_of_scheduled_jobs.append(storage_queue.enqueue(process_cv, curr_content))
+    list_of_scheduled_jobs.append(STORAGE_QUEUE.enqueue(process_cv, curr_content))
 
-time.sleep(3)
+time_slept_in_secs = 0
+while not all_jobs_done(list_of_scheduled_jobs):
+    print(f"Waited {time_slept_in_secs} seconds for all jobs to finish ...")
+    time.sleep(1)
+    time_slept_in_secs += 1
 
 for curr_job in list_of_scheduled_jobs:
     print(f">>>>>>>>>>>> {curr_job.result}")
